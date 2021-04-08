74dbf65aa29db4ed1c7427d59d8d08653f44398f
Tom <18027761141@163.com>
2021-04-08 11:20:25 +0800

    Initial commit

---
+++ src/main/java/cn/wyu/dbdocument/SpringBootMainTest.java
@@ -0,0 +1,11 @@
+package cn.wyu.dbdocument;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootMainTest {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMainTest.class,args);
+    }
+}
